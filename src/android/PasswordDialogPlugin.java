c8bd2d16c7b0a7d7412a475360d28bf25464a8ff
rogerben <40767658+rogerben@users.noreply.github.com>
2018-08-21 00:14:05 +0800

    Update PasswordDialogPlugin.java

--- src/android/PasswordDialogPlugin.java
+++ src/android/PasswordDialogPlugin.java
@@ -515,7 +515,7 @@ public final class PasswordDialogPlugin extends CordovaPlugin {
         // Perform validation.
 
         if (minLength != -1 && password.length() < minLength) {
-            String message = String.format("The password needs to be at least %d characters long.", minLength);
+            String message = String.format("Minimum %d characters long / 最少 %d 个字符", minLength);
             etPassword.setError(message);
             etPassword.requestFocus();
             return;
@@ -669,7 +669,7 @@ public final class PasswordDialogPlugin extends CordovaPlugin {
         if (minLength != -1 && password.length() < minLength) {
             etPassword.setText("");
             etConfirmPassword.setText("");
-            String message = String.format("The password needs to be at least %d characters long.", minLength);
+            String message = String.format("Minimum %d characters long / 最少 %d 个字符", minLength);
             etPassword.setError(message);
             etPassword.requestFocus();
             return;
@@ -845,7 +845,7 @@ public final class PasswordDialogPlugin extends CordovaPlugin {
         if (minLength != -1 && newPassword.length() < minLength) {
             etNewPassword.setText("");
             etConfirmPassword.setText("");
-            String message = String.format("The new password needs to be at least %d characters long.", minLength);
+            String message = String.format("Minimum %d characters long / 最少 %d 个字符", minLength);
             etNewPassword.setError(message);
             etNewPassword.requestFocus();
             return;
